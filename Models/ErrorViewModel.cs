40985fae725ad9bfe122ab6bd2b1679945a294be
Leonardo Esteller <lesteller@hotmail.com>
2026-01-19 13:07:18 -0600

    Primer commit SuperChikis

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ListaSupermercado.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
