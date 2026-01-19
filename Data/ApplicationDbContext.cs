d2c4b2edc1013ad4e75f1f2885c3bf2c694bea98
lesteller34 <lesteller@hotmail.com>
2026-01-19 16:13:13 -0600

    ajuste#
    Migración a EF Core y PostgreSQL, elimina Identity/Sqlite
    
    Se elimina IdentityDbContext y dependencias de Identity y Sqlite.
    Se añade soporte para Entity Framework Core y PostgreSQL.
    Actualización de UserSecretsId en el proyecto.
    Nuevo perfil de lanzamiento WSL para desarrollo en WSL2.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using ListaSupermercado.Models;
 
 namespace ListaSupermercado.Data;
 
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
